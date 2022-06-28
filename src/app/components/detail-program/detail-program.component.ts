86c04608844d3db42e9cfcb596141f23e22611fc
Andrea Lorena Castro Manzano <lcastro0398@gmail.com>
2022-06-28 00:46:21 -0500

    Cursos
    Mostrar cursos agregados en los detalles por programa

--- src/app/components/detail-program/detail-program.component.ts
+++ src/app/components/detail-program/detail-program.component.ts
@@ -3,6 +3,7 @@ import { ActivatedRoute } from '@angular/router'
 import { ProgramI } from 'src/app/shared/modals/program-i'
 import { ProgramService } from 'src/app/shared/services/program.service'
 import { FullProgramI } from '../../shared/modals/fullProgram-i';
+import { CourseI } from '../../shared/modals/course-i';
 
 @Component({
   selector: 'app-detail-program',
@@ -11,6 +12,7 @@ import { FullProgramI } from '../../shared/modals/fullProgram-i';
 })
 export class DetailProgramComponent implements OnInit {
   fullProgram: FullProgramI | undefined
+  courses: CourseI[] | undefined
 
   constructor(
     private programService: ProgramService,
@@ -28,7 +30,7 @@ export class DetailProgramComponent implements OnInit {
   getProgram(id: string): void {
     this.programService.getProgram(id).subscribe((data) => {
       this.fullProgram = data;
-      //aca van los cursos this.answers = data.answers;
+      this.courses = data.program.courses;
     })
   }
 
